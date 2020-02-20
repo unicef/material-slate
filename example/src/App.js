fc8ab2c759765a1e3f5e0dbab7acf939127b8500
sureshsevarthi <sevarthi.suresh@gmail.com>
2020-02-20 11:25:09 -0500

    updated the example page

--- example/src/App.js
+++ example/src/App.js
@@ -2,16 +2,44 @@ import React, { useState } from 'react'
 import LinksExample from './examples/Links'
 import HoveringToolbarExample from './examples/HoverToolbar'
 import RichTextExample from './examples/RichText.js'
-import { Grid } from '@material-ui/core'
+import { Grid, Typography, Link } from '@material-ui/core'
 
 export default function App() {
   return (
-    <Grid mt={8} container>
-      {/* <Button onClick={e => setComments([])}>Reset</Button> */}
+    <Grid container spacing={3}>
+      <Grid item direction="column">
+        <Typography variant="h6" style={{ paddingLeft: 8 }}>
+          RichText{' '}
+          <Link variant="body1" href="/">
+            [View source]
+          </Link>
+        </Typography>
+        {/* <Button onClick={e => setComments([])}>Reset</Button> */}
 
-      <RichTextExample />
-      <HoveringToolbarExample />
-      <LinksExample />
+        <RichTextExample />
+      </Grid>
+      <Grid item direction="column">
+        <Typography variant="h6" style={{ paddingLeft: 8 }}>
+          Hovering Toolbar{' '}
+          <Link variant="body1" href="/">
+            [View source]
+          </Link>
+        </Typography>
+        {/* <Button onClick={e => setComments([])}>Reset</Button> */}
+
+        <HoveringToolbarExample />
+      </Grid>
+      <Grid item direction="column">
+        <Typography variant="h6" style={{ paddingLeft: 8 }}>
+          Custom Hook (Links){' '}
+          <Link variant="body1" href="/">
+            [View source]
+          </Link>
+        </Typography>
+        {/* <Button onClick={e => setComments([])}>Reset</Button> */}
+
+        <LinksExample />
+      </Grid>
     </Grid>
   )
 }
