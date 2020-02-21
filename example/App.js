e8019b1e879fa95dc05e65ec616237029130c400
merlos <merlos@users.noreply.github.com>
2020-02-21 10:49:23 -0500

    added hot loader

--- example/App.js
+++ example/App.js
@@ -1,13 +1,14 @@
 
-import React from "react";
-import { RichSlate } from '../src';
+import React from "react" 
+import {hot} from "react-hot-loader" //Auto refresh load
+import { RichSlate } from '../src'
 
-export default function App() {
+export default hot(module)(function App() {
     return(
         <div className="App">
-        <h1> Hello, World! </h1>
+        <h1> Hello, World!</h1>
         <RichSlate></RichSlate>
       </div>
     );
-}
+})
 
