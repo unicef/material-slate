57f2354b31d125b1f97ef41d4e6e5fe0c700ed60
merlos <merlos@users.noreply.github.com>
2020-02-23 08:08:06 -0500

    Add hotkey Support

--- src/components/RichEditable.jsx
+++ src/components/RichEditable.jsx
@@ -1,8 +1,9 @@
 import React, { useCallback } from 'react'
-import { Editable } from 'slate-react'
+import { Editable, useSlate } from 'slate-react'
 import PropTypes from 'prop-types'
 import { Box } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core'
+import isHotkey from 'is-hotkey'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -10,7 +11,7 @@ const useStyles = makeStyles(theme => ({
     paddingRight: theme.spacing(1),
     paddingBottom: theme.spacing(1),
     fontFamily: theme.typography.fontFamily
-    
+
   }
 }))
 
@@ -18,8 +19,33 @@ const useStyles = makeStyles(theme => ({
  * Wrapper of Slate Editable
  *  
  */
-export default function RichEditable({ renderElement, renderLeaf, placeholder, children }) {
+export default function RichEditable({ renderElement, renderLeaf, placeholder, hotkeys, onHotkey, children }) {
 
+  const editor = useSlate()
+
+  const defaultHotKeys = {
+    'mod+b': {
+      type: 'mark',
+      value: 'bold'
+    },
+    'mod+i': {
+      type: 'mark',
+      value: 'italic',
+    },
+    'mod+u': {
+      type: 'mark',
+      value: 'underlined'
+    },
+    'mod+`': {
+      type: 'mark',
+      value: 'code'
+    }
+  }
+
+  const allHotkeys = {
+    ...defaultHotKeys,
+    ...hotkeys,
+  }
   const classes = useStyles()
 
   // Define a rendering function based on the element passed to `props`. 
@@ -63,31 +89,58 @@ export default function RichEditable({ renderElement, renderLeaf, placeholder, c
     return renderLeaf({ leaf, attributes, children, rest })
   }, [])
 
-  return (
-    <Box className={classes.root}>
-      <Editable
-        renderElement={props => handleRenderElement(props)}
-        renderLeaf={props => handleRenderLeaf(props)}
-        placeholder={placeholder}
-      >{children}</Editable>
-    </Box>
-  )
-}
+  const handleOnKeyDown = (event) => {
+    for (const pressedKeys in allHotkeys) {
+      if (isHotkey(pressedKeys, event)) {
+        const hotkey = allHotkeys[pressedKeys]
+        console.log(pressedKeys)
+        console.log(hotkey)
+        event.preventDefault()
+        if (hotkey.type === 'mark') {
+          editor.toggleMark(hotkey.value)
+        }
+        if (hotkey.type === 'block') {
+          editor.toggleBlock(hotkeys.value)
+        }
+        return onHotkey && onHotkey({ event, editor, hotkey, pressedKeys, allHotkeys })
+      }
+    }
+  }
+    return (
+      <Box className={classes.root}>
+        <Editable
+          renderElement={props => handleRenderElement(props)}
+          renderLeaf={props => handleRenderLeaf(props)}
+          onKeyDown={event => handleOnKeyDown(event)}
+          placeholder={placeholder}
+        >{children}</Editable>
+      </Box>
+    )
+  }
 
-// Specifies the default values for props:
-RichEditable.defaultProps = {
-  renderElement: ({ element, attributes, children, ...rest }) => { return <p {...attributes}>{children}</p> },
-  renderLeaf: ({ leaf, attributes, children, ...rest }) => { return <span {...attributes}>{children}</span> },
-  placeholder: "Type some text..."
-};
+  // Specifies the default values for props:
+  RichEditable.defaultProps = {
+    renderElement: ({ element, attributes, children, ...rest }) => { return <p {...attributes}>{children}</p> },
+    renderLeaf: ({ leaf, attributes, children, ...rest }) => { return <span {...attributes}>{children}</span> },
+    placeholder: "Type some text..."
+  };
 
-// TODO add info about arguments in functions
+  // TODO add info about arguments in functions
 
-RichEditable.propTypes = {
-  /** Called when an element needs to be rendered */
-  renderElement: PropTypes.func,
-  /** Called when a leaf needs to be rendered */
-  renderLeaf: PropTypes.func,
-  /** Text to display when there are no contents on the editor. Default" "Type some text..." */
-  placeholder: PropTypes.string
-}
+  RichEditable.propTypes = {
+    /** Called when an element needs to be rendered */
+    renderElement: PropTypes.func,
+    /** Called when a leaf needs to be rendered */
+    renderLeaf: PropTypes.func,
+    /** Text to display when there are no contents on the editor. Default" "Type some text..." */
+    placeholder: PropTypes.string,
+    /** 
+     * Additional hotkeys to be added other than default. Object of the form `{'mod+k': {type: 'mark', value: 'italic'}  
+     */
+    hotkeys: PropTypes.object,
+    /** 
+     * Event tht will be triggered in case a hotkey is detected 
+     * It has one single argument that can be deconstructed in `{event, editor, hotkey, pressedKeys, allHotkeys}`
+     */
+    onHotKey: PropTypes.func
+  }
