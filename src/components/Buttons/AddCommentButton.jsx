12b873205325457e7c1625a7ab284786ceab282b
merlos <merlos@users.noreply.github.com>
2020-04-03 21:17:32 -0400

    disabled buttons when they should not be enabled based on the current editor selection

--- src/components/Buttons/AddCommentButton.jsx
+++ src/components/Buttons/AddCommentButton.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useSlate } from 'slate-react'
+import { Range } from 'slate'
 import ToolbarButton from './ToolbarButton'
 import AddCommentOutlinedIcon from '@material-ui/icons/AddCommentOutlined'
 
 const AddCommentButton = React.forwardRef(
-  (props, ref) => (
-  <ToolbarButton icon={<AddCommentOutlinedIcon />} type="other" tooltip="Add comment" format="comment" ref={ref} {...props} />
-  )
+  (props, ref) => {
+    const editor = useSlate()
+
+    /**
+     * The comment button is disabled when the selection is collapsed (ie there is no selection)
+     */
+    const isDisabled = () => {
+      return editor.isSelectionCollapsed()
+    }
+
+    return (
+      <ToolbarButton icon={<AddCommentOutlinedIcon />} type="other" disabled={isDisabled()} tooltip="Add comment" format="comment" ref={ref} {...props} />
+    )
+  }
 )
 export default AddCommentButton  
--- src/components/Buttons/EndnoteButton.jsx
+++ src/components/Buttons/EndnoteButton.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useSlate } from 'slate-react'
 import ToolbarButton from './ToolbarButton'
 import CallToActionOutlinedIcon from '@material-ui/icons/CallToActionOutlined'
 
 const EndnoteButton = React.forwardRef(
-  (props, ref) => (
-  <ToolbarButton icon={<CallToActionOutlinedIcon />} type="other" tooltip="Add endnote" format="endnote" ref={ref} {...props} />
-  )
+  (props, ref) => { 
+    const editor = useSlate()
+
+    //Only is enabled if there is some text selected
+    const isDisabled = () => {
+      return editor.isSelectionExpanded()
+    }
+    return (
+      <ToolbarButton icon={<CallToActionOutlinedIcon />} type="other" disabled={isDisabled()} tooltip="Add endnote" format="endnote" ref={ref} {...props} />
+    )
+  }
 )
 export default EndnoteButton  
