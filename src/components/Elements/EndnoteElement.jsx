ac15d5d7048724a9002ddbdd510c75a81ea90a66
merlos <merlos@users.noreply.github.com>
2020-04-04 20:21:26 -0400

    Add documentation of EndnoteElement

--- src/components/Elements/EndnoteElement.jsx
+++ src/components/Elements/EndnoteElement.jsx
@@ -8,6 +8,15 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
+/**
+ * Displays a super index text with the index number of the endnote. 
+ * A tooltip with the content of the endnote is displayed if the user hovers the endnote.
+ * 
+ * Expects the `element` prop to have `element.data.body` the text of the endnote (string) and
+ * `element.data.index` the index number fo the endnote.
+ *  
+ * If `onClick` prop is set it is called if user clicks the tex
+ */
 const EndnoteElement = ({ element, onClick, attributes, children }) => {
   const classes = useStyles()
   return (
