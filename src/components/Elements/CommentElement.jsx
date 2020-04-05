0c53f0de85958b1eb7043235fb369ac828dea72c
merlos <merlos@users.noreply.github.com>
2020-04-04 20:17:41 -0400

    Add documentation to CommentElement

--- src/components/Elements/CommentElement.jsx
+++ src/components/Elements/CommentElement.jsx
@@ -9,6 +9,16 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
+/**
+ * Renders a Comment. 
+ *  1. Adds blueish background to the text the comment wraps
+ *  2. On hover displays the comment.
+ * 
+ *  If `onClick` is passed, it is called if the text wrapped by the comment is clicked.
+ * 
+ *  Expects the `element` object passed as prop to have `element.data.body` to display the comment text.
+ * 
+ */
 const CommentElement = ({ element, onClick, children, attributes }) => {
   const classes = useStyles()
   return (
