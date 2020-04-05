c3514cf71deb3d1a89e1197dcf7dc8ee66df11f5
merlos <merlos@users.noreply.github.com>
2020-04-05 08:09:08 -0400

    Gave some format to main page example

--- example/src/App.js
+++ example/src/App.js
@@ -1,22 +1,54 @@
 
 import React  from "react"
+
+import { makeStyles} from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+
 import Basic from './Basic'
 import Hovering from './Hovering'
 import Advanced from './Advanced'
 
+const useStyles = makeStyles(theme => ({
+  intro: {
+    marginLeft: theme.spacing(2),
+    paddingLeft: theme.spacing(2)
+  },
+  example: {
+    margin: theme.spacing(2),
+    padding: theme.spacing(2)
+  },
+  readable: {
+    maxWidth: 800
+  }
+}))
+
 export default function App() {
+  const classes = useStyles()
   return (
-     <div className="App">
-       <h2>Advanced usage </h2>
-       <a href="http://url">Source code</a>
+    <>
+      <Box className={classes.intro}>
+       <Typography variant="h1">UNICEF Material Slate</Typography>
+       <Typography>A simple rich text editor for React that uses Material UI and Slate</Typography>
+       <Typography variant="h4"><a href='https://github.com/unicef/material-slate/'>View on Github</a></Typography>
+       </Box>
+      <Box className={`${classes.example} ${classes.readable} ` }>
+      <Typography variant="h2">Basic Editor Example</Typography>
+          <a href="https://github.com/unicef/material-slate/blob/master/example/src/Basic.js">View source code</a>
+          <Basic />
+      </Box>
+
+      <Box className={`${classes.example} ${classes.readable} ` }>
+      <Typography variant="h2">Hover toolbar</Typography>
+      <Typography>Toolbar appears on selecting a text</Typography>
+          <a href="https://github.com/unicef/material-slate/blob/master/example/src/Hovering.js">View source code</a>
+          <Hovering />
+      </Box>
+
+      <Box className={classes.example}></Box>
+      <Typography variant="h2">Advanced usage </Typography>
+       <a href="https://github.com/unicef/material-slate/blob/master/example/src/Advanced.js">View source code</a>
        <Advanced />
-      <h2>Basic Editor Example</h2>
-      <a href="http://url">Source code</a>
-      <Basic />
-      <h2>Hover toolbar</h2>
-      <p>Toolbar appears on selecting a text</p>
-      <a href="http://url">Source code</a>
-      <Hovering />
-      </div>
+      </>
    );
 }
