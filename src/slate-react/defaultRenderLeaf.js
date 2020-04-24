09a049511b5da4cec42f34c08434643e5802ae25
merlos <merlos@users.noreply.github.com>
2020-04-24 18:53:06 -0400

    extracted render leaf from MaterialSlate

---
+++ src/slate-react/defaultRenderLeaf.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+/**
+ * Default renderer of leafs. 
+ * 
+ * Handles the following type of leafs `bold` (strong), `code` (code), `italic` (em), `strikethrough` (del), `underlined`(u).
+ * 
+ * @param {Object} props 
+ */
+
+export default function defaultRenderLeaf({ leaf, attributes, children, ...rest }) {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>
+  }
+  if (leaf.code) {
+    children = <code>{children}</code>
+  }
+  if (leaf.italic) {
+    children = <em>{children}</em>
+  }
+  if (leaf.strikethrough) {
+    children = <del>{children}</del>
+  }
+  if (leaf.underlined) {
+    children = <u>{children}</u>
+  }
+  return <span {...attributes}>{children}</span> 
+}
