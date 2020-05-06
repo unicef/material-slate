fea7fc8c6b966e07528e18731751c91e392e26ad
Suresh <ssevarthi@unicef.org>
2020-05-06 14:03:10 +0530

    Added serialize and syncExternalNodes in Base plugin

--- src/plugins/withBase.js
+++ src/plugins/withBase.js
@@ -1,22 +1,20 @@
-
 import MaterialEditor from '../slate/MaterialEditor'
 import { Range } from 'slate'
 import { Transforms } from 'slate'
 import { Location } from 'slate'
 
 /**
- * 
+ *
  * Base plugin for Material Slate.
- * 
+ *
  * All other plugins assume this plugin exists and has been included.
- * 
- * @param {Editor} editor 
+ *
+ * @param {Editor} editor
  */
 const withBase = editor => {
-
   /**
    * Is the current editor selection a range, that is the focus and the anchor are different?
-   * 
+   *
    * @returns {boolean} true if the current selection is a range.
    */
   editor.isSelectionExpanded = () => {
@@ -26,14 +24,14 @@ const withBase = editor => {
   /**
    * Returns true if current selection is collapsed, that is there is no selection at all
    * (the focus and the anchor are the same).
-   * 
+   *
    * @returns {boolean} true if the selection is collapsed
    */
   editor.isSelectionCollapsed = () => {
-    return ! editor.isSelectionExpanded()
+    return !editor.isSelectionExpanded()
   }
 
-  /** 
+  /**
    * Is the editor focused?
    * @returns {boolean} true if the editor has focus. */
   editor.isFocused = () => {
@@ -48,9 +46,9 @@ const withBase = editor => {
   }
 
   /**
-   * 
+   *
    * @param {string} type type of node to be checked. Example: `comment`, `numbered-list`
-   * 
+   *
    * @returns {bool} true if within current selection there is a node of type `type`
    */
   editor.isNodeTypeActive = type => {
@@ -65,8 +63,8 @@ const withBase = editor => {
 
   /**
    * Gets current selection and stores it in rememberedSelection.
-   * 
-   * This may be useful when you need to open a dialog box and the editor loses the focus 
+   *
+   * This may be useful when you need to open a dialog box and the editor loses the focus
    */
   editor.rememberCurrentSelection = () => {
     editor.rememberedSelection = editor.selection
@@ -84,18 +82,17 @@ const withBase = editor => {
   /**
    * Wraps a selection with an argument. If `wrapSelection` is not passed
    * uses current selection
-   * 
+   *
    * Upon wrapping moves the cursor to the end.
-   * 
+   *
    * @param {Node} node the node to be added
    * @param {Selection} wrapSelection selection of the text that will be wrapped with the node.
-   * 
+   *
    */
-  editor.wrapNode = (node, wrapSelection = null ) => {
-    
+  editor.wrapNode = (node, wrapSelection = null) => {
     //if wrapSelection is passed => we use it. Use editor selection in other case
-    editor.selection = wrapSelection ?  wrapSelection : editor.selection
-     
+    editor.selection = wrapSelection ? wrapSelection : editor.selection
+
     // if the node is already wrapped with current node we unwrap it first.
     if (editor.isNodeTypeActive(node.type)) {
       editor.unwrapNode(node.type)
@@ -109,25 +106,25 @@ const withBase = editor => {
     } else {
       //text is selected => add the node
       Transforms.wrapNodes(editor, node, { split: true })
-      console.log("editor", editor.children)
+      console.log('editor', editor.children)
       Transforms.collapse(editor, { edge: 'end' })
     }
   }
 
   /**
    * Unwraps or removes the nodes that are not in the list.
-   * 
+   *
    * It will search for all the nodes of `type` in the editor and will keep only
    * the ones in the nodesToKeep.
-   * 
-   * It assumes each item of nodesToKeep has an attribute `id`. This attribute will be the discriminator.  
-   * 
+   *
+   * It assumes each item of nodesToKeep has an attribute `id`. This attribute will be the discriminator.
+   *
    */
   editor.syncExternalNodes = (type, nodesToKeep, unwrap = true) => {
     //extracts the id from the nodes and removes those that are not in the list
-    const listOfIds = nodesToKeep.map( node => node.id)
-    
-    if (unwrap) { 
+    const listOfIds = nodesToKeep.map(node => node.id)
+
+    if (unwrap) {
       editor.unwrapNotInList(type, listOfIds)
     } else {
       editor.removeNotInList(type, listOfIds)
@@ -135,22 +132,25 @@ const withBase = editor => {
     const nodesToKeepObj = {}
     //Update values of nodes.data
     //Create a map by id of the nodes to keep
-    nodesToKeep.forEach(node => nodesToKeepObj[node.id] = node)
+    nodesToKeep.forEach(node => (nodesToKeepObj[node.id] = node))
     //Find nodes of this type remaining in the editor
     const editorNodes = editor.findNodesByType(type)
     //Update them
     editorNodes.map(node => {
-      Transforms.setNodes(editor, {data: nodesToKeepObj[node.id]}, {match: n => n.id == node.id, at: []}) 
+      Transforms.setNodes(
+        editor,
+        { data: nodesToKeepObj[node.id] },
+        { match: n => n.id == node.id, at: [] }
+      )
     })
-    
   }
-  
+
   /**
    * Removes the nodes that are not in the list of Ids
-   * 
+   *
    * Nodes of type `type` shall have the attribute/property `id`
-   * 
-   * Example: 
+   *
+   * Example:
    * ```
    * {
    *    type: `comment`
@@ -160,33 +160,32 @@ const withBase = editor => {
    * ```
    */
   editor.removeNotInList = (type, listOfIds) => {
-    Transforms.removeNodes(editor, { 
-      match: n => (n.type === type) && (! listOfIds.includes(n.id)),
-      at: [] //Search the whole editor content 
-     })
+    Transforms.removeNodes(editor, {
+      match: n => n.type === type && !listOfIds.includes(n.id),
+      at: [], //Search the whole editor content
+    })
   }
 
   /**
-   * 
+   *
    * Unwraps the nodes of `type` whose ids are not in the provided list
-   * 
+   *
    * It assumes the nodes of `type` have an attribute `id`. The `id` may be a number or string.
-   * 
+   *
    * @param {string} type node type to be searched. Example: `comment`
    * @param {Array} listOfIds Array with the list of ids. Example: [1, 2, 3].
    */
   editor.unwrapNotInList = (type, listOfIds) => {
-    
-     Transforms.unwrapNodes(editor, { 
-      match: n => (n.type === type) && (! listOfIds.includes(n.id)),
-      at: [] //Search the whole editor content 
-     })
+    Transforms.unwrapNodes(editor, {
+      match: n => n.type === type && !listOfIds.includes(n.id),
+      at: [], //Search the whole editor content
+    })
   }
-  
+
   /**
    * Gets from current editor content the list of items of a particular type
    */
-  editor.findNodesByType = (type) => {
+  editor.findNodesByType = type => {
     const list = MaterialEditor.nodes(editor, {
       match: n => n.type === type,
       at: [],
@@ -199,6 +198,68 @@ const withBase = editor => {
     })
     console.log('fondNodesByType ', listWithNodes)
     return listWithNodes
+  }
+
+  /**
+   * Returns the serialized value (plain text)
+   */
+  editor.serialize = nodes => {
+    return nodes.map(n => Node.string(n)).join('\n')
+  }
+
+  /**
+   * Functions similar to syncExternalNodes,and also updates the node temporaryId with original id and data
+   *
+   * First, It will search for match in temporaryId's in nodesToKeep with id's of nodes and updates it with latest data
+   * Then, updates data in node id's matching with nodesToKeep id's
+   *
+   * Unwraps or removes the nodes that are not in the list.
+   */
+  editor.syncExternalNodesWithTemporaryId = (
+    type,
+    nodesToKeep,
+    unwrap = true
+  ) => {
+    //extracts the id from the nodes and removes those that are not in the list
+    const listOfIds = nodesToKeep.map(node => node.id)
+
+    const nodesToKeepObj = {}
+    //Update values of nodes.data
+    //Create a map by id of the nodes to keep
+    nodesToKeep.forEach(node => (nodesToKeepObj[node.id] = node))
+    //Find nodes of this type remaining in the editor
+    const editorNodes = editor.findNodesByType(type)
+    //Update them
+    editorNodes.map(node => {
+      // Find the key of node to update
+      const key = Object.keys(nodesToKeepObj).find(
+        key => nodesToKeepObj[key].temporaryId === node.id
+      )
+
+      // node to Update with original Id and data
+      const nodeToUpdate = nodesToKeepObj[key]
+      // If node.id exists
+      if (nodesToKeepObj[node.id] && !nodeToUpdate) {
+        Transforms.setNodes(
+          editor,
+          { data: nodesToKeepObj[node.id] },
+          { match: n => n.id == node.id, at: [0] }
+        )
+        // TemporaryId and data will be replaced with new id and data
+      } else if (key && nodeToUpdate) {
+        Transforms.setNodes(
+          editor,
+          { id: nodeToUpdate.id, data: nodeToUpdate },
+          { match: n => n.id == nodeToUpdate.temporaryId, at: [] }
+        )
+      } else if (unwrap) {
+        // unwraps the nodes in not list
+        editor.unwrapNotInList(type, listOfIds)
+      } else {
+        // removes the nodes in not list
+        editor.removeNotInList(type, listOfIds)
+      }
+    })
   }
 
   return editor
