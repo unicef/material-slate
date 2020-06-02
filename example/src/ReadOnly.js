818cc3edef7fef42e95999b76b2549b5038ccb18
merlos <merlos@users.noreply.github.com>
2020-06-02 10:05:12 -0400

    added read only mode

---
+++ example/src/ReadOnly.js
@@ -0,0 +1,32 @@
+
+import React from "react"
+import { useState, useMemo } from "react"
+import {
+  MaterialSlate,
+  MaterialEditable,
+  createMaterialEditor,
+  Toolbar,
+  HoveringToolbar
+} from '@unicef/material-slate' 
+
+//Initial contents of the editor
+import initialValue from './initialValue'
+
+/**
+ * Basic Material Slate example
+ */
+export default function ReadOnly() {
+  // Holds the value of the editor
+  const [value, setValue] = useState(initialValue())
+
+  // An instance of material editor. It is an slate editor with a few more functions
+  const editor = useMemo(() => createMaterialEditor(), [])
+
+  return (
+      <MaterialSlate editor={editor} value={value} onChange={(value) => setValue(value)}>
+        <Toolbar></Toolbar>
+        <HoveringToolbar></HoveringToolbar>
+        <MaterialEditable readOnly={true}/>
+      </MaterialSlate>
+  )
+}
