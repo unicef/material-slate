c6e24ed5a80f5db632bec3f228259e83159afd01
Suresh <sevarthi.suresh@gmail.com>
2020-07-24 19:06:06 -0400

    Implemneted Link Button for toolbar

---
+++ src/components/Buttons/LinkButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ToolbarButton from './ToolbarButton'
+import LinkIcon from '@material-ui/icons/Link'
+
+/**
+ * Toolbar button for adding links
+ *
+ * @see ToolbarButton
+ */
+const LinkButton = React.forwardRef((props, ref) => (
+  <ToolbarButton
+    icon={<LinkIcon />}
+    type="link"
+    tooltip="Add link"
+    format="link"
+    ref={ref}
+    {...props}
+  />
+))
+export default LinkButton
