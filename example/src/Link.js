22769e157c60283089e0586ee83b1effe3e896a0
Suresh <sevarthi.suresh@gmail.com>
2020-07-27 10:55:11 -0400

    Link button to have all the logic to enter url

--- example/src/Link.js
+++ example/src/Link.js
@@ -7,7 +7,6 @@ import {
   Toolbar,
   withLinks,
   LinkButton,
-  SimpleDialog,
 } from '@unicef/material-slate'
 
 //Initial contents of the editor
@@ -19,26 +18,8 @@ import initialValue from './initialValue'
 export default function Link() {
   // Holds the value of the editor
   const [value, setValue] = useState(initialValue())
-  // Handles the dialog that is opened upon clicking the Link Toolbar/HoveringBar button
-  const [openLinkDialog, setOpenLinkDialog] = useState(false)
-
   // An instance of material editor. It is an slate editor with a few more functions
   const editor = useMemo(() => withLinks(createMaterialEditor()), [])
-
-  // Handles custom buttons click
-  const onLinkButtonDown = ({ editor }) => {
-    // When the dialog box is opened, the focus is lost as well as current selected text.
-    // We need to save it for later on.
-    editor.rememberCurrentSelection()
-    setOpenLinkDialog(true)
-  }
-
-  const handleDialogSave = url => {
-    setOpenLinkDialog(false)
-    // Adds the link to the editor.
-    // The link will wrap the selected text when `rememberCurrentSelection()` was called
-    editor.insertLink(url)
-  }
 
   return (
     <>
@@ -48,18 +29,10 @@ export default function Link() {
         onChange={value => setValue(value)}
       >
         <Toolbar>
-          <LinkButton onMouseDown={event => onLinkButtonDown(event)} />
+          <LinkButton />
         </Toolbar>
         <MaterialEditable allowHotKeys={false} />
       </MaterialSlate>
-      <SimpleDialog
-        open={openLinkDialog}
-        title="Add Link"
-        label="Link"
-        format="link"
-        onCancel={() => setOpenLinkDialog(false)}
-        onSave={({ value }) => handleDialogSave(value)}
-      />
     </>
   )
 }
--- src/components/Buttons/LinkButton.jsx
+++ src/components/Buttons/LinkButton.jsx
@@ -1,20 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSlate } from 'slate-react'
 import ToolbarButton from './ToolbarButton'
 import LinkIcon from '@material-ui/icons/Link'
+import SimpleDialog from '../SimpleDialog'
 
 /**
  * Toolbar button for adding links
  *
  * @see ToolbarButton
  */
-const LinkButton = React.forwardRef((props, ref) => (
-  <ToolbarButton
-    icon={<LinkIcon />}
-    type="link"
-    tooltip="Add link"
-    format="link"
-    ref={ref}
-    {...props}
-  />
-))
-export default LinkButton
+export default function LinkButton({ ref, ...props }) {
+  const editor = useSlate()
+  // Handles the dialog that is opened upon clicking the Link Toolbar/HoveringBar button
+  const [openLinkDialog, setOpenLinkDialog] = useState(false)
+
+  // Handles custom buttons click
+  const onLinkButtonDown = ({ editor }) => {
+    // When the dialog box is opened, the focus is lost as well as current selected text.
+    // We need to save it for later on.
+    editor.rememberCurrentSelection()
+    setOpenLinkDialog(true)
+  }
+
+  const handleDialogSave = url => {
+    setOpenLinkDialog(false)
+    // Adds the link to the editor.
+    // The link will wrap the selected text when `rememberCurrentSelection()` was called
+    editor.insertLink(url)
+  }
+
+  return (
+    <React.Fragment>
+      <ToolbarButton
+        icon={<LinkIcon />}
+        type="link"
+        tooltip="Add link"
+        format="link"
+        ref={ref}
+        onMouseDown={event => onLinkButtonDown(event)}
+        {...props}
+      />
+      <SimpleDialog
+        open={openLinkDialog}
+        title="Add Link"
+        label="Link"
+        format="link"
+        onCancel={() => setOpenLinkDialog(false)}
+        onSave={({ value }) => handleDialogSave(value)}
+      />
+    </React.Fragment>
+  )
+}
