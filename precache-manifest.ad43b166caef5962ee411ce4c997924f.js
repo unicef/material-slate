e7d016282bc77f9e2604a22f977b13c81c04e229
merlos <merlos@users.noreply.github.com>
2020-09-25 16:39:51 -0400

    Updates

---
+++ precache-manifest.ad43b166caef5962ee411ce4c997924f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff19fe70a32394241f7f8d7acf1af6e",
+    "url": "/material-slate/index.html"
+  },
+  {
+    "revision": "09fafef8e199a2b029e2",
+    "url": "/material-slate/static/js/2.fe4e45c4.chunk.js"
+  },
+  {
+    "revision": "36e3cb7b900796cae71de6313f78c388",
+    "url": "/material-slate/static/js/2.fe4e45c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c834bf69d5d54f518df",
+    "url": "/material-slate/static/js/main.7d79d87b.chunk.js"
+  },
+  {
+    "revision": "9bc4300f3d534c7c9724",
+    "url": "/material-slate/static/js/runtime-main.bc327887.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/material-slate/precache-manifest.7f463cba0a05845449f75f49377b7895.js"
+  "/material-slate/precache-manifest.ad43b166caef5962ee411ce4c997924f.js"
 );
 
 self.addEventListener('message', (event) => {
